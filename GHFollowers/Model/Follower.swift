72369c81ff7ac14242b7ed6b3ca8e80bd9ad9c73
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-08-25 15:49:56 +0300

    Add Follower and User models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Mohamed Elbendary on 25/08/2026.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+}
