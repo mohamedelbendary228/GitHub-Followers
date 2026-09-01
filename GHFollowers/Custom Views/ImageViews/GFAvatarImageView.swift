da39f4a05c8806bafa2cd75cf3d2a79e132447b4
mohamedelbendary <mohamedelbendary228@gmail.com>
2026-09-01 14:59:24 +0300

    create a custom FollowerCell and GFAvatarImageView

---
+++ GHFollowers/Custom Views/ImageViews/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollowers
+//
+//  Created by Mohamed Elbendary on 29/08/2026.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = Images.placeholder
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
